b74a43cea4e60d7b62b92cbf82d9ff3ad6cc6bc6
abdurafeyf <abdurafeyf@gmail.com>
2022-12-05 12:50:11 +0500

    initial

--- GreenBee.py
+++ GreenBee.py
@@ -5,8 +5,10 @@ import numpy as np
 from fpdf import FPDF
 import fpdf
 import base64
-# import plotly.express as px
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from io import StringIO, BytesIO
+from PIL import Image
+from matplotlib.figure import Figure
 # import firebase_admin
 # from firebase_admin import credentials
 # from firebase_admin import db
@@ -640,6 +642,32 @@ pdf.cell(200, 10, txt = f"Size of Invertor/Charge Calculator", ln=29)
 pdf.set_font("Arial", size = 13)
 pdf.cell(200, 10, txt= f"Size of invertor: {((watthrsum)+(watthrsum*(additional_load/100))) / (invertor_eff/100)}", ln=30)
 pdf.cell(200, 10, txt = f"Solar Controller: {t/24} AMP", ln =31)
+
+
+fig = Figure(figsize=(6, 4), dpi=300)
+fig.subplots_adjust(top=0.8)
+ax1 = fig.add_subplot(211)
+ax1.set_ylabel("volts")
+ax1.set_title("a sine wave")
+
+t = np.arange(0.0, 1.0, 0.01)
+s = np.sin(2 * np.pi * t)
+(line,) = ax1.plot(t, s, color="blue", lw=2)
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+ax2 = fig.add_axes([0.15, 0.1, 0.7, 0.3])
+n, bins, patches = ax2.hist(
+    np.random.randn(1000), 50, facecolor="yellow", edgecolor="yellow"
+)
+ax2.set_xlabel("time (s)")
+
+# Converting Figure to an image:
+canvas = FigureCanvas(fig)
+canvas.draw()
+img = Image.fromarray(np.asarray(canvas.buffer_rgba()))
+pdf.image(img, w=pdf.epw)  # Make the image full width
 
 with st.sidebar:
     st.image(image='profile2.jpeg')
