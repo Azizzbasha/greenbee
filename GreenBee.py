46b52fbff459b4ecc26d4157907c0b2c306a12f1
abdurafeyf <abdurafeyf@gmail.com>
2022-12-05 12:52:47 +0500

    initial

--- GreenBee.py
+++ GreenBee.py
@@ -644,30 +644,12 @@ pdf.cell(200, 10, txt= f"Size of invertor: {((watthrsum)+(watthrsum*(additional_
 pdf.cell(200, 10, txt = f"Solar Controller: {t/24} AMP", ln =31)
 
 
-fig = Figure(figsize=(6, 4), dpi=300)
-fig.subplots_adjust(top=0.8)
-ax1 = fig.add_subplot(211)
-ax1.set_ylabel("volts")
-ax1.set_title("a sine wave")
-
-t = np.arange(0.0, 1.0, 0.01)
-s = np.sin(2 * np.pi * t)
-(line,) = ax1.plot(t, s, color="blue", lw=2)
-
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-ax2 = fig.add_axes([0.15, 0.1, 0.7, 0.3])
-n, bins, patches = ax2.hist(
-    np.random.randn(1000), 50, facecolor="yellow", edgecolor="yellow"
-)
-ax2.set_xlabel("time (s)")
-
-# Converting Figure to an image:
-canvas = FigureCanvas(fig)
-canvas.draw()
-img = Image.fromarray(np.asarray(canvas.buffer_rgba()))
-pdf.image(img, w=pdf.epw)  # Make the image full width
+plt.figure(figsize=[2, 2])
+x = np.arange(0, 10, 0.00001)
+y = x*np.sin(2* np.pi * x)
+plt.plot(y)
+plt.savefig("figure.svg", format="svg")
+pdf.image("figure.svg")
 
 with st.sidebar:
     st.image(image='profile2.jpeg')
