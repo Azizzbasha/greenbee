756e3cc77c62843cccf077642229aeda613dd3f8
abdurafeyf <abdurafeyf@gmail.com>
2022-12-05 12:22:32 +0500

    plot

--- GreenBee.py
+++ GreenBee.py
@@ -123,17 +123,6 @@ st.dataframe(df)
 if st.button('Generate Graph'):
     # st.area_chart(df, x='Total Watt', y='Watt Hr/Day')
     st.bar_chart(df, x = 'Total Watt', y='Watt Hr/Day')
-    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    y = [5, 7, 8, 1, 4, 9, 6, 3, 5, 2, 1, 8]
-    
-    plt.plot(x, y)
-    
-    plt.xlabel('x-axis label')
-    plt.ylabel('y-axis label')
-    plt.title('Matplotlib Example')
-    plot = "output.jpg"
-    plt.savefig(plot,"output.jpg")
-    st.image(plot)
     # st.set_option('deprecation.showPyplotGlobalUse', False)
     # totalwattgraph = [i for i in df['Total Watt']]
     # watthrdaygraph = [i for i in df['Watt Hr/Day']]
@@ -146,6 +135,16 @@ if st.button('Generate Graph'):
 
 # from firebase_admin import db
 
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+y = [5, 7, 8, 1, 4, 9, 6, 3, 5, 2, 1, 8]
+
+plt.plot(x, y)
+
+plt.xlabel('x-axis label')
+plt.ylabel('y-axis label')
+plt.title('Matplotlib Example')
+plt.savefig('output.png')
+plt.show()
 # ref = db.reference("/Load Details/")
 
 # totalwattsum = ref.order_by_child("totalwttsum".get())
