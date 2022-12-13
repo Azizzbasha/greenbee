063025814220dc47eb802522de2596e9e3d2b521
abdurafeyf <abdurafeyf@gmail.com>
2022-12-13 18:28:45 +0500

    graph

--- GreenBee.py
+++ GreenBee.py
@@ -650,10 +650,10 @@ pdf.cell(200, 10, txt = f"Solar Controller: {t/24} AMP", ln =33)
 
 fig = plt.figure(figsize = (10, 7))
 plt.bar(df['Total Watt'], df['Watt Hr/Day'], align='center')
-for i in range(len(df)):
-    plt.axhline(y=df['Watt Hr/Day'][i], color='b', linestyle='-', xmax=0)
-for i, v in enumerate(df['Watt Hr/Day']):
-    fig.text(v + 3, i + .25, str(v), color = 'blue', fontweight='bold')
+# for i in range(len(df)):
+#     plt.axhline(y=df['Watt Hr/Day'][i], color='b', linestyle='-', xmax=0)
+# for i, v in enumerate(df['Watt Hr/Day']):
+#     fig.text(v + 3, i + .25, str(v), color = 'blue', fontweight='bold')
 plt.xlabel('Total Watt')
 plt.ylabel('Watt Hr/Day')
 plt.title('Total Watt vs Watt Hr/Day')
