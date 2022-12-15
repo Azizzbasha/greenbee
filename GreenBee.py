6082ee0f72299e0df8766469eb8741246764192a
abdurafeyf <abdurafeyf@gmail.com>
2022-12-15 16:40:45 +0500

    pass

--- GreenBee.py
+++ GreenBee.py
@@ -685,10 +685,10 @@ with st.sidebar:
     username = username.text_input("Username")
     password = st.empty()
     password = password.text_input("Password")
-    password.empty()
     if st.button('Login'):
         login_button = True
 if login_button:
     for key in userpass:
             if (username == key and password == userpass[key]):
                 run_app()
+                st.runtime.legacy_caching.clear_cache()
